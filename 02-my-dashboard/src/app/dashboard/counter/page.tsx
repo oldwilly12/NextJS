a6b57459744725d72d7d9ad721fc30f7cbc620d2
Fracisco Acosta <rwilly203@gmail.com>
2024-11-29 09:56:28 -0600

    finishing with the counter
    finishing with the counter and using use client and different tools

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,15 +1,17 @@
+import { CarCounter } from "@/app/shopping-cart";
+
+
+export const metadata = {
+ title: 'Shopping Cart',
+ description: 'Just a Counter',
+};
 
 export default function CounterPage() {
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">10</span>
-
-      <div>
-        <button className="flex items-center">
-          +1
-        </button>
-      </div>
+      <CarCounter valor = {10} />
     </div>
   );
 }
---
+++ 02-my-dashboard/src/app/shopping-cart/components/CarCounter.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from "react";
+
+interface Props {
+    valor?: number;
+}
+
+export const CarCounter = ({valor = 0} : Props) => {
+
+    const [count, setCount] = useState(valor);
+
+    return (
+        <>
+            <span className="text-9xl">{count}</span>
+
+            <div className="flex">
+                <button
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={() => setCount(count + (-1))} >
+                    -1
+                </button>
+                <button
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={() => setCount(count + 1)} >
+                    +1
+                </button>
+            </div>
+        </>
+    )
+}
