4fcf4c9aff870699abe5f1a37d6e513e77835235
Fracisco Acosta <rwilly203@gmail.com>
2024-11-29 08:43:57 -0600

    starting the counter
    starting the counter

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,15 @@
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">10</span>
+
+      <div>
+        <button className="flex items-center">
+          +1
+        </button>
+      </div>
     </div>
   );
 }
