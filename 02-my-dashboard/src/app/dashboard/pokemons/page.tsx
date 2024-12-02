ceabbd1d1016b3354a36af8b01b75f8684effae6
Fracisco Acosta <rwilly203@gmail.com>
2024-12-02 17:56:36 -0600

    little changes
    littles changes pokemons

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,31 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from '@/app/pokemons';
+import Image from 'next/image';
 
-export default function PokemonsPage() {
+
+const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+  .then(res => res.json());
+    
+
+
+  const pokemons = data.results.map( pokemon => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name,
+  }))
+
+  return pokemons;
+};
+
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons(151);
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className='flex flex-col'>
+
+      <span className='text-5xl my-2'>Listado de pokemons <small>estatico</small></span>
+      < PokemonGrid pokemons={pokemons} />
+      
     </div>
   );
 }
---
+++ 02-my-dashboard/src/app/pokemons/index.ts
@@ -0,0 +1,4 @@
+export { PokemonGrid } from './components/PokemonGrid';
+
+export type { SimplePokemon } from './interfaces/simple-pokemons';
+export type { PokemonsResponse} from './interfaces/pokemons-response';
