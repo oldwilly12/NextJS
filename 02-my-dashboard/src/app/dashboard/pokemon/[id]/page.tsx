c606fe91c7b95fc78c99bcddfd060e79a8c72ed0
Fracisco Acosta <rwilly203@gmail.com>
2024-12-03 17:22:16 -0600

    acabando url dinamico
    acabando url dinamico

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,23 +1,33 @@
+import { Pokemon } from "@/pokemons";
 
 interface Props {
     params: {id: string},
 }
 
-const getPokemon = async(id: string) => {
+
+export const metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+};
+
+const getPokemon = async(id: string): Promise<Pokemon> => {
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
         cache: 'force-cache' // TODO: cambiar a futuro
     }).then(res => res.json());
 
-    console.log(pokemon);
+    console.log('Se cargo: ', pokemon.name);
+
+    return pokemon;
 }
 
-export default function PokemonPage({ params }: Props) {
+export default async function PokemonPage({ params }: Props) {
 
-    const pokemon = getPokemon(params.id);
+    const pokemon = await getPokemon(params.id);
 
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div>{JSON.stringify( pokemon )}</div>
     </div>
   );
 }
--- 02-my-dashboard/src/pokemons/index.ts
+++ 02-my-dashboard/src/pokemons/index.ts
@@ -2,3 +2,4 @@ export { PokemonGrid } from './components/PokemonGrid';
 
 export type { SimplePokemon } from './interfaces/simple-pokemons';
 export type { PokemonsResponse} from './interfaces/pokemons-response';
+export type { Pokemon } from './interfaces/pokemon';
